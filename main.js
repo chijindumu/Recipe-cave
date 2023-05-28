86b2e5dc0622afed6dbb6b59b8da9065c89ddca1
Joshua Benjamin <117050813+chijindumu@users.noreply.github.com>
2023-05-28 16:49:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const menu = document.querySelector('#menu');
+const button = document.querySelector('#button');
+const more = document.querySelector('#more');
+const loadMore = document.querySelector('#loadMore');
+
+menu.style.display = 'none';
+//more.style.display = 'none';
+
+button.onclick = function(){
+  if(menu.style.display == 'none'){
+    menu.style.display = 'block'
+  }else{
+    menu.style.display = 'none'
+  };
+};
+/*loadMore.onclick = function(){
+  if(more.style.display == 'none'){
+    more.style.display = 'block'
+    loadMore.innerHTML = "View Less"
+  }else{
+    more.style.display = 'none'
+    loadMore.innerHTML = "load more"
+  };
+};*/
